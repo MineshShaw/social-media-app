bebfd3366be17f7811580c3971b952300c6c92ce
MineshShaw <mineshshaw2007@gmail.com>
2025-09-01 10:53:07 +0530

    initial setup

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/dbconnection.js";
+
+dotenv.config({quiet: true, path: ".env"});
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
